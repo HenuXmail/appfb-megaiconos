3c801d697c06e4829e0c1a24625a04112de2bf5f
Jimmy Miller <henux@marmota.(none)>
2012-07-16 00:51:51 -0400

    Se documentan funciones y se organiza el codigo, se crean las miniaturas en base a la imagen recortada por el usuario

--- crop.php
+++ crop.php
@@ -1,26 +1,102 @@
 <?php
 
 /**
- * Jcrop image cropping plugin for jQuery
- * Example cropping script
- * @copyright 2008-2009 Kelly Hallman
- * More info: http://deepliquid.com/content/Jcrop_Implementation_Theory.html
+ * Recorte de imagenes y mini-imagenes
+ * @copyright sitioweb3.com
+ * @author Jmiller
+ * @author kerosene
  */
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
-	$targ_w = $targ_h = 150;
-	$jpeg_quality = 90;
-
-	$src = $_POST['url'];
-	$img_r = imagecreatefromjpeg($src);
-	$dst_r = ImageCreateTrueColor( $targ_w, $targ_h );
-
-	imagecopyresampled($dst_r,$img_r,0,0,$_POST['x'],$_POST['y'],
-	$targ_w,$targ_h,$_POST['w'],$_POST['h']);
-
-	header('Content-type: image/jpeg');
-	imagejpeg($dst_r,null,$jpeg_quality);
-
+	//TODO crear validaciones correspondientes
+	
+	$src 	= 	$_POST['url'];
+	$x		=	$_POST['x'];
+	$y		=	$_POST['y'];
+	$w		=	$_POST['w'];
+	$h		=	$_POST['h'];
+	
+	//Se recorta la imagen del usuario 90x90
+	$imagen	=	recortar_imagen($src, 'henux', $x, $y, $h, $w);
+	//Se crean las miniaturas en base al recorte anteriorna
+	$imagenes = crear_miniatura($imagen, 15, 15, 6, 6, 'henux');
+	print_r($imagenes);
 	exit;
+}
+
+/**
+ * Funcrion que recorta la imagen seleccionada por el usuario
+ *
+ * @return string URL de la imagen recortada
+ * @param string URL de la imagen original
+ * @author  Jmiller
+ */
+function recortar_imagen($url,$usuario,$x,$y,$h,$w) {
+	
+	//TODO Eliminar imagenes en memoria
+	$targ_w = $targ_h = 90;
+	$jpeg_quality = 90;
+	$nombre	=	time().$usuario;
+	
+	$origen = imagecreatefromjpeg($url);
+	$destino = ImageCreateTrueColor( $targ_w, $targ_h );
+
+	imagecopyresampled($destino,$origen,0,0,$x,$y,
+							$targ_w,$targ_h,$w,$h);
+
+	//header('Content-type: image/jpeg');
+	imagejpeg($destino,'iconos/'.$nombre.'.jpg',$jpeg_quality);
+	
+	return 'iconos/'.$nombre.'.jpg';
+}
+
+/**	
+ * Funcion que crea y devuelve miniaturas de una imagen
+ * 
+ * Devuelve un array con la informacion de las imagenes creadas, como primer
+ * valor, se encuentra la imagen original para ser usada a modo de 'preview' y
+ * las demás imagenes corresponden a los recortes de la imagen
+ * 
+ * @author Jmiller
+ * @param string $url direccion de la imagen a recortar
+ * @param integer $ancho Tamaño del ancho de las miniaturas
+ * @param integer alto $alto de las miniaturas
+ * @param integer $c_horizontal	Cantidad de miniaturas a recortar horizontalmente
+ * @param integer $c_vertical Cantidad de miniaturas a recortar verticalmente
+ * @param string $usuario Codigo de usuario a agregar a las imagenes
+ * @return array primer valor es la imagen principal, y los demás, las urls de las miniaturas creadas
+ */
+function crear_miniatura($url, $ancho, $alto, $c_horizontal, $c_vertical, $usuario){
+	
+	//TODO Eliminar imagenes en memoria
+	$jpeg_quality = 90;
+	$nombre		=	time().$usuario;
+	
+	$origen 	= imagecreatefromjpeg($url);
+	$destino 	= ImageCreateTrueColor( $ancho, $alto );
+	
+	$imagenes 	= array();
+	$imagenes[] = $url;
+	$contador	= 0;
+	$avance_x	= 0;
+	$avance_y	= 0;
+	for ($i=0; $i < $c_vertical; $i++) {
+		//Reiniciamos el contador a 0 para comenzar desde la primera columna
+		$avance_x = 0; 
+		for ($j=0; $j < $c_horizontal; $j++) {
+			$contador++; 
+			//Se saca la porcion de la imagen (recortamos)
+			imagecopy($destino, $origen, 0, 0, $avance_x, $avance_y, $ancho, $alto);
+			//Se guarda la imagen previamente recortada
+			imagejpeg($destino,'iconos/'.$nombre.'-'.$contador.'.jpg',$jpeg_quality);
+			// Guardamos la imagen en el array para obtenerla mas tarde
+			$imagenes[] = 'iconos/'.$nombre.'-'.$contador.'.jpg';
+			//Avanzamos para recortar el cuadrado del costado derecho
+			$avance_x = $avance_x + 15;
+		}
+		//Avanzamos para pasar a la fila siguiente
+		$avance_y = $avance_y + 15;
+	}
+	return $imagenes;
 }
